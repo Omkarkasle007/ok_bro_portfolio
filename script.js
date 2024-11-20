330428c2e1d8061d4b6914c2130fd01332e56a60
omkar kasle <omkarkasle2003@gmail.com>
2024-11-20 13:11:25 +0530

    link and update js file

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const h1 = document.getElementById("head1");
 
-h1.addEventListener("hover", () => {
+h1.addEventListener("mouseover", (e) => {
   h1.style.color = "white";
 });
+
+h1.addEventListener("mouseout", (e) => {
+  h1.style.color = "black";
+});
+
+console.log("ok");
